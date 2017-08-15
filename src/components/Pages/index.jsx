2a4d5abbb6227ba097e2e634e562ef5ade4962b6
ino_h <syslink.h.inoue@gmail.com>
2017-08-15 15:35:26 +0900

    スライドページを追加

--- src/components/Pages/index.jsx
+++ src/components/Pages/index.jsx
@@ -5,9 +5,12 @@ import Cover from '../Cover';
 import Syntax from '../Syntax';
 import List, { Item } from '../List';
 import Image from '../Image';
+import { H1, H2 } from '../Caption';
+
 import Hello from './Hello';
 
 import trend from './images/trend.png';
+import helloWorld from './images/hello-world.png';
 
 const Pages = () => (
   <div className="swiper-wrapper">
@@ -16,12 +19,14 @@ const Pages = () => (
     </Slide>
 
     <Slide>
+      <H2>Reactのイメージ</H2>
+      <p>少しググってみた...</p>
       <List title="ポジティブ">
         <Item>モダンでカッコイイ</Item>
         <Item>早いらしい</Item>
       </List>
       <List title="ネガティブ">
-        <Item>jsxがきもい</Item>
+        <Item>jsxが気持ち悪い</Item>
         <Item>環境作るのが難しい</Item>
       </List>
     </Slide>
@@ -31,7 +36,7 @@ const Pages = () => (
     </Slide>
 
     <Slide>
-      <Cover>jsxがきもい<br />（理由）</Cover>
+      <Cover>jsxが気持ち悪い<br />（理由）</Cover>
     </Slide>
 
     <Slide>
@@ -50,14 +55,15 @@ const Pages = () => (
 
     <Slide>
       <List title="Web Components">
-        <Item>HTMLの要素としてWebの部品として定義する仕様</Item>
+        <Item>Webの部品として要素を定義する共通仕様</Item>
         <Item>js, html, css をひとまとまりの部品として管理提供</Item>
         <Item>jsのフレームワークではあたりまえの機能になってきた</Item>
       </List>
     </Slide>
 
     <Slide>
-      <p>いろいろsample</p>
+      <p>いろんな hello world!</p>
+      <p>公式サイトから部分的に拝借</p>
       <Hello />
     </Slide>
 
@@ -77,16 +83,31 @@ const Pages = () => (
       <Syntax lang="html">{`Vue.component('hello', {
   template: '<button @click="onClick">Hello World!!</button>',
   methods: {
-    onClick: function () {
-      alert('hello');
-    }
+    onClick: function () { alert('hello'); }
   },
 })`}</Syntax>
     </Slide>
 
     <Slide>
+      <p>Vue.js(Vueify)</p>
+      <Syntax>{`<template>
+  <button @click="onClick">Hello World!!</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    onClick() { alert('hello'); },
+  },
+}
+</script>`}</Syntax>
+    </Slide>
+
+    <Slide>
       <p>Power of `ES6 + React`</p>
-      <Syntax>{`const Hello = () => <button onClick={() => alert('hello')}>Hello World!!</button>`}</Syntax>
+      <Syntax>{`const Hello = () => (
+  <button onClick={() => alert('hello')}>Hello World!!</button>
+);`}</Syntax>
     </Slide>
 
     <Slide>
@@ -109,9 +130,13 @@ const Pages = () => (
     </Slide>
 
     <Slide>
+      <List title="Reactができること">
+        <Item>ViewをRenderする</Item>
+        <Item>コンポーネントを管理（制御）する</Item>
+      </List>
       <List title="Reactだけじゃできないこと">
-        <Item>テスト</Item>
         <Item>cssやjavascriptのクロスコンパイル</Item>
+        <Item>テスト</Item>
         <Item>ajax</Item>
         <Item>routing</Item>
         <Item>etcetc...</Item>
@@ -120,6 +145,15 @@ const Pages = () => (
 
     <Slide>
       <Cover>create-react-app</Cover>
+    </Slide>
+
+    <Slide>
+      <H2>create-react-app</H2>
+      <div>
+        設定なしで最新のビルドセットアップを提供。<br />
+        単一ページのReactアプリケーションを作成するために公式にサポートされている。
+      </div>
+      <p>※公式サイトの文言を直訳</p>
     </Slide>
 
     <Slide>
@@ -134,23 +168,137 @@ const Pages = () => (
     </Slide>
 
     <Slide>
-      <List title="足りなもの">
-        <Item>Fluxライブラリ(Redux)</Item>
-        <Item>Ajax通信</Item>
-        <Item>Router</Item>
+      <H2>ここからは動かしながら確認</H2>
+      <p>動かす内容はスライドを確認</p>
+      <p>http://ino-h.com/react-swiper-sample/</p>
+    </Slide>
+
+    <Slide>
+      <H2>インストール</H2>
+      <Syntax>{`npm install -g create-react-app # gem install rails
+
+create-react-app my-app # rails new my-app
+cd my-app/
+npm start # rails s`}</Syntax>
+    </Slide>
+
+    <Slide>
+      <Image src={helloWorld}></Image>
+    </Slide>
+
+    <Slide>
+      <H2>`src/App.jsx` を修正する</H2>
+      <Syntax>{`import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return <button onClick={() => alert('hello')}>Hello World!!</button>
+  }
+}
+
+export default App;
+`}</Syntax>
+    </Slide>
+
+    <Slide>
+      <H2>Componentを作る</H2>
+      <Syntax>{`import React from 'react';
+
+const Hello = () => (
+  <button onClick={() => alert('hello')}>Hello World!!</button>
+);
+
+export default Hello;
+`}</Syntax>
+    </Slide>
+
+    <Slide>
+      <H2>Componentを組み込む</H2>
+      <Syntax>{`import React, { Component } from 'react';
+import Hello from './Hello';
+
+class App extends Component {
+  render() {
+    ~~return <button onClick={() => alert('hello')}>Hello World!!</button>~~
+    return <Hello />
+  }
+}
+
+export default App;`}</Syntax>
+    </Slide>
+
+
+    <Slide>
+      <H2>Componentにプロパティを追加する</H2>
+      <Syntax>{`import React, { PropTypes } from 'react';
+
+const Hello = ({ value, children }) => (
+  <button onClick={() => alert(value)}>{children}</button>
+);
+
+Hello.propTypes = {
+  value: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Hello;
+`}</Syntax>
+    </Slide>
+
+    <Slide>
+      <H2>Componentのプロパティを適用する</H2>
+      <Syntax>{`import React, { Component } from 'react';
+import Hello from './Hello';
+
+class App extends Component {
+  render() {
+    ~~return <Hello />~~
+    return <Hello value="hello">Hello world!!!</Hello>
+  }
+}
+
+export default App;`}</Syntax>
+    </Slide>
+
+    <Slide>
+      <List title="create-react-appに足りなもの">
+        <Item>Ajax通信 # => FetchAPI, axios, superagent, jQuery ...</Item>
+        <Item>Router # => react-router</Item>
+        <Item>Fluxライブラリ(Redux) # => Redux, Flux</Item>
+        <Item>CSS周り # => CSSModules, sass, stylus</Item>
       </List>
     </Slide>
 
     <Slide>
-      <List title="">
+      <Cover>CSS周り</Cover>
+    </Slide>
+
+    <Slide>
+      <List title="sassを追加する">
+        <Item>create-react-appの公式サイトを見るとsassの追加の仕方が書いてある</Item>
+        <Item>CSSModulesは未対応らしい（現状ejectが必要？）</Item>
       </List>
     </Slide>
 
     <Slide>
-      <Cover>sassを追加する</Cover>
+      <H2>必要なパッケージを追加する</H2>
+      <Syntax>{`# ファイルを監視してsassをコンパイル
+npm install --save node-sass-chokidar
+# npmのコマンドでsassのコンパイルを追加するために必要
+npm install --save npm-run-all`}</Syntax>
     </Slide>
 
     <Slide>
+      <H2>sassファイルを追加する</H2>
+      <Syntax>{`.hello {
+  &__button {
+    background-color: red;
+  }
+}`}</Syntax>
+    </Slide>
+
+    <Slide>
+      <H2>sassをコンパイルするスクリプトを追加する</H2>
       <Syntax>{`  "scripts": {
   +    "build-css": "node-sass-chokidar src/ -o src/",
   +    "watch-css": "npm run build-css && node-sass-chokidar src/ -o src/ --watch --recursive",
@@ -162,6 +310,22 @@ const Pages = () => (
       "test": "react-scripts test --env=jsdom",
       "eject": "react-scripts eject"
     }`}</Syntax>
+    </Slide>
+
+    <Slide>
+      <H2>定義したcssをhtmlに追加する</H2>
+      <Syntax>{` ~~ 略 ~~
+import './index.css';
+
+const Hello = ({ value, children }) => (
+  <button
+    className="hello__button"
+    onClick={() => alert(value)}
+  >
+    {children}
+  </button>
+);
+ ~~ 略 ~~`}</Syntax>
     </Slide>
   </div>
 );
